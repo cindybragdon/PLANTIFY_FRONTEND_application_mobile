1e30339b174883e9e75eee6b259871e073788b75
root <root@equipe1.10.10.0.5>
2024-12-06 21:36:25 +0000

    initial commit

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1" >
+      <Text className="text-center"> Plantify </Text>
+      
+    </View>
+  );
+}
+
